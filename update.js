3941c8a84a20f119f52280f6cc63a73088c82af9
AzSiAz <slider.perso@live.fr>
2018-11-21 00:03:10 +0100

    add pagination

--- update.js
+++ update.js
@@ -1,6 +1,8 @@
 const fetch = require("node-fetch").default
 const cheerio = require("cheerio")
-const micro = require("micro")
+const { send } = require("micro")
+const { parse } = require("url")
+
 
 const extractSlug = (url, regex) => {
     const match = url.match(regex);
@@ -8,10 +10,28 @@ const extractSlug = (url, regex) => {
     return match ? match[1] : "";
 };
 
+const extractPagination = ($) => {
+    const maxPage = parseInt($("div.digg_pagination > a").last().prev("a").text(), 10) || 1;
+    const currentPage = parseInt($("div.digg_pagination > em.current").text(), 10) || 1;
+    const nextPage = parseInt(
+        $("div.digg_pagination > a").first().text(),
+        10,
+    ) || 1;
+
+    return {
+        currentPage,
+        maxPage,
+        nextPage
+    };
+};
+
+
 module.exports = async (req, res) => {
-    console.time("update")
+    // console.time("update")
     try {
-        const request = await fetch("https://www.novelupdates.com")
+        const params = parse(req.url, true).query
+        url = params.page ? `https://www.novelupdates.com/?pg=${params.page}` : "https://www.novelupdates.com"
+        const request = await fetch(url)
         const html = await request.text()
         const $ = cheerio.load(html)
 
@@ -36,11 +56,12 @@ module.exports = async (req, res) => {
                 releaseLink: `https:${releaseLink}`
             };
         }).get()
+        const pagination = extractPagination($)
 
-        console.timeEnd("update")
-        return micro.send(res, 200, updates)
+        // console.timeEnd("update")
+        return send(res, 200, { updates, pagination })
     } catch (error) {
-        return micro.send(res, 500, error.message)
+        return send(res, 500, error.message)
     }
 
 }
