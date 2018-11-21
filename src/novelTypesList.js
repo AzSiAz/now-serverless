b8215bf4e4dff9ace14dabdfc7eb4a0e6d8bf558
AzSiAz <slider.perso@live.fr>
2018-11-22 00:50:10 +0100

    add novel types list

---
+++ src/novelTypesList.js
@@ -0,0 +1,56 @@
+// @ts-check
+const fetch = require("node-fetch").default
+const cheerio = require("cheerio")
+const { send, createError } = require("micro")
+const { parse } = require("url")
+
+const extractPagination = require("./utils/extractPagination")
+const { extractSlug, novelTypesRegex } = require("./utils/extractSlug")
+const escapeCustom = require("./utils/escapeCustom")
+
+/**
+ * @param {CheerioStatic} $
+ * @returns {object[]}
+ */
+const extractNovelTypesData = ($) => {
+    return $("div.wpb_wrapper > ul > li > a").map((i, el) => {
+        const element = $(el)
+
+        return {
+            name: escapeCustom(element.text().trim()),
+            slug: extractSlug(element.attr("href"), novelTypesRegex)
+        }
+    }).get()
+}
+
+/**
+ * @param {string} requestUrl
+ * @returns {Promise<CheerioStatic>}
+ */
+const requestData$ = async (requestUrl) => {
+    const params = parse(requestUrl, true).query
+    const url = `https://www.novelupdates.com/list-types/?pg=${params.page}`
+    const request = await fetch(url)
+    const html = await request.text()
+
+    return cheerio.load(html)
+}
+
+/**
+ * @param {import("http").IncomingMessage} req
+ * @param {import("http").ServerResponse} res
+ */
+module.exports = async (req, res) => {
+    try {
+        const $ = await requestData$(req.url)
+        const pagination = extractPagination($)
+        const data = extractNovelTypesData($)
+
+        send(res, 200, {
+            pagination,
+            data
+        })
+    } catch (error) {
+        send(res, 500, error.message)
+    }
+}
